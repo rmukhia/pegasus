4f9ad9e7b292239a8e49ce7ca82eb89937807453
Raunak Mukhia <raunak.mukhia@gmail.com>
2020-05-08 00:38:47 +0700

    Application cradle.

--- pegasus-net-sim/NS3PegasusApp.cc
+++ pegasus-net-sim/NS3PegasusApp.cc
@@ -1,13 +1,56 @@
 
 #include "NS3PegasusApp.h"
 #include "PegasusSocket.h"
+#include "ns3/log.h"
+
+NS_LOG_COMPONENT_DEFINE ("NS3PegasusApp");
+NS_OBJECT_ENSURE_REGISTERED (NS3PegasusApp);
+
+void NS3PegasusApp::StartApplication(void ) {
+  NS_LOG_FUNCTION(this);
+}
+
+void NS3PegasusApp::StopApplication(void ) {
+  NS_LOG_FUNCTION(this);
+}
+
+void NS3PegasusApp::DoDispose(void ) {
+  NS_LOG_FUNCTION(this);
+  Application::DoDispose();
+}
+
+TypeId NS3PegasusApp::GetTypeId(void )
+{
+   static TypeId tid = TypeId ("ns3::NS3PegasusApp")
+     .SetParent<Application> ()
+     .SetGroupName("Applications")
+     .AddConstructor<NS3PegasusApp> ()
+     .AddTraceSource ("Rx", "A packet has been received",
+                      MakeTraceSourceAccessor (&NS3PegasusApp::m_rxTrace),
+                      "ns3::Packet::TracedCallback")
+     .AddTraceSource ("RxWithAddresses", "A packet has been received",
+                      MakeTraceSourceAccessor (&NS3PegasusApp::m_rxTraceWithAddresses),
+                      "ns3::Packet::TwoAddressTracedCallback")
+   ;
+   return tid;
+}
+
+NS3PegasusApp::NS3PegasusApp() {
+  NS_LOG_FUNCTION(this);
+}
+
+NS3PegasusApp::~NS3PegasusApp() {
+  NS_LOG_FUNCTION(this);
+}
 
 void NS3PegasusApp::ScheduleSend() {
+  NS_LOG_FUNCTION(this);
 }
 
 void NS3PegasusApp::Send() {
+  NS_LOG_FUNCTION(this);
 }
 
 void NS3PegasusApp::HandleRead() {
+  NS_LOG_FUNCTION(this);
 }
-
--- pegasus-net-sim/NS3PegasusApp.h
+++ pegasus-net-sim/NS3PegasusApp.h
@@ -4,22 +4,44 @@
 
 #include <vector>
 
-#include <ns3/socket.h>
-#include <ns3/application.h>
+#include "ns3/application.h"
+#include "ns3/event-id.h"
+#include "ns3/ptr.h"
+#include "ns3/address.h"
+#include "ns3/socket.h"
+#include "ns3/traced-callback.h"
 
 using namespace ns3;
 
 class PegasusSocket;
 
 //Inherits from ns3::Application Module.
-class NS3PegasusApp {
+class NS3PegasusApp: public Application {
   private:
     std::vector<Ptr<Socket>> m_socket;
 
+    TracedCallback<Ptr<const Packet>, const Address &, const Address &> m_rxTraceWithAddresses;
+
+    TracedCallback<Ptr<const Packet> > m_rxTrace;
+
     PegasusSocket& FindPegasusSocket(const Ptr<Node> & node, const unsigned int & simSockIndex);
+
+    virtual void StartApplication(void );
+
+    virtual void StopApplication(void );
+
+
+  protected:
+    virtual void DoDispose(void );
 
 
   public:
+    static TypeId GetTypeId(void );
+
+    NS3PegasusApp();
+
+    virtual ~NS3PegasusApp();
+
     void ScheduleSend();
 
     void Send();
